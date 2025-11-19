7ce1aae66e4711b7dcd41e8d9eef00137db3723a
Ahmad <ma7574110@gmail.com>
2025-11-19 13:05:05 +0500

    Changes in delete Account

--- lib/post_app_screens/profile_details/profile_details_controller.dart
+++ lib/post_app_screens/profile_details/profile_details_controller.dart
@@ -1,3 +1,4 @@
+import 'package:application_new/models/post_model.dart';
 import 'package:application_new/models/user_model_post_app.dart';
 import 'package:application_new/post_app_screens/login_screen/login_screen.dart';
 import 'package:application_new/utils/user_base_controller.dart';
@@ -9,8 +10,50 @@ import 'package:flutter/material.dart';
 class ProfileDetailsController extends GetxController {
   TextEditingController nameController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+  List<UserModelPostApp> users = [];
+  List<PostModel> posts = [];
   bool isLoading = false;
   String? password;
+
+  void getUsers() async {
+    try {
+      final user =
+          await FirebaseFirestore.instance
+              .collection(UserModelPostApp.tableName)
+              .get();
+      if (user.docs.isNotEmpty) {
+        users =
+            user.docs.map((e) {
+              return UserModelPostApp.fromMap(e.data());
+            }).toList();
+        update();
+      }
+    } catch (e) {
+      Get.dialog(
+        AlertDialog(title: Text('Error'), content: Text(e.toString())),
+      );
+    }
+  }
+
+  void getPosts() async {
+    try {
+      final post =
+          await FirebaseFirestore.instance
+              .collection(PostModel.tableName)
+              .get();
+      if (post.docs.isNotEmpty) {
+        posts =
+            post.docs.map((e) {
+              return PostModel.fromMap(e.data());
+            }).toList();
+        update();
+      }
+    } catch (e) {
+      Get.dialog(
+        AlertDialog(title: Text('Error'), content: Text(e.toString())),
+      );
+    }
+  }
 
   void updateUserData() async {
     try {
@@ -85,12 +128,25 @@ class ProfileDetailsController extends GetxController {
                             .collection(UserModelPostApp.tableName)
                             .doc(UserBaseController.userData.id)
                             .delete();
+                        for (var e in posts) {
+                          List<String> likedBYUpdated = e.likedBy ?? [];
+                          if (likedBYUpdated.contains(
+                            UserBaseController.userData.id,
+                          )) {
+                            likedBYUpdated.remove(
+                              UserBaseController.userData.id,
+                            );
+                            await FirebaseFirestore.instance
+                                .collection(PostModel.tableName)
+                                .doc(e.id)
+                                .update({'likedBy': likedBYUpdated});
+                          }
+                        }
                         await FirebaseAuth.instance.currentUser!.delete();
                         Get.offAll(() => LoginScreen());
                         update();
                       }
                     },
-
                     child: Text(
                       'Confirm',
                       style: TextStyle(color: Colors.redAccent),
@@ -111,6 +167,8 @@ class ProfileDetailsController extends GetxController {
 
   @override
   void onInit() {
+    getUsers();
+    getPosts();
     nameController.text = UserBaseController.userData.userName ?? "";
     super.onInit();
   }
